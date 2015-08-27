557918ea19602dad4cde8e6bf6b566df207471ef
Nick Johnstone <ncwjohnstone@gmail.com>
2015-08-27 19:38:55 +1200

    Add labels with hax

--- index.js
+++ index.js
@@ -44,6 +44,10 @@ function calculateValuePosition (currentTime, streamValue) {
 function renderStreamValue (currentTime, streamValue) {
   const left = calculateValuePosition(currentTime, streamValue);
 
+  if (left < -10) {
+    return null;
+  }
+
   return (
     h('.stream-value', {style: {left: left + '%'}}, JSON.stringify(streamValue.value))
   );
@@ -51,20 +55,25 @@ function renderStreamValue (currentTime, streamValue) {
 
 function renderStream (currentTime, streamValues) {
   return (
-    h('.stream',
-      streamValues.map(renderStreamValue.bind(null, currentTime))
-    )
+    h('.stream', [
+      h('.stream-title', streamValues.label),
+      ...streamValues.map(renderStreamValue.bind(null, currentTime))
+    ])
   );
 }
 
 function logStreams (streams) {
-  const loggedStreams = streams.map(stream => {
-    return stream
+  const loggedStreams = streams.map(streamInfo => {
+    return streamInfo.stream
       .startWith([])
       .scan((events, newEvent) => {
-        return events.concat([
+        const newEvents = events.concat([
           {occurredAt: getCurrentTime(), value: newEvent}
         ]);
+
+        newEvents.label = streamInfo.label;
+
+        return newEvents;
       });
   });
 
@@ -86,7 +95,11 @@ function main ({DOM}) {
   const userIntent = intent(DOM);
   const count$ = model(userIntent);
 
-  const streamLogs = logStreams([count$]);
+  const streamLogs = logStreams([
+    {stream: count$, label: 'count'},
+    {stream: count$.debounce(600), label: 'count.debounce(600ms)'},
+    {stream: count$.sample(600), label: 'count.sample(600ms)'},
+  ]);
   const app = view(count$);
 
   return {
