9d9c5e4fafe90a8472db66c72dfc702d2f0e7678
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-01 18:57:34 +1300

    Only display source stream
    Add a TODO to get all source streams

--- src/time-travel.js
+++ src/time-travel.js
@@ -17,7 +17,8 @@ function run (main, drivers) {
   const timeTravelDOMDriver = makeDOMDriver(timeTravelBarNode);
   const [requests, responses] = Cycle.run(main, drivers);
 
-  const streamsToDisplay = [requests.DOM.source].concat(requests.DOM.source.sources.s).map((stream, index) => (
+  // TODO - walk tree of stream sources
+  const streamsToDisplay = [requests.DOM.source].map((stream, index) => (
     {stream: stream, label: index.toString()}
   ))
 
