155a126b00a6289ae8d9332c18478de79adcf23e
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-03 22:46:19 +1300

    Add a failing test for time travel

--- test/time-travel.js
+++ test/time-travel.js
@@ -7,6 +7,23 @@ const {h, makeDOMDriver} = require('@cycle/dom');
 
 const TimeTravel = require('../src/time-travel');
 
+function MouseEvent(type, dict) {
+  var e = document.createEvent("MouseEvents");
+
+  dict = dict || {};
+
+  let bubbles = false;
+  let catchable = false;
+
+  // This is deprecated, but lol phantom mmk https://github.com/ariya/phantomjs/issues/11289
+  e.initMouseEvent(type, bubbles, catchable, window, 0, dict.clientX, dict.clientY, dict.clientX, dict.clientY);
+
+  e.clientX = dict.clientX;
+  e.clientY = dict.clientY;
+
+  return e;
+}
+
 function createRenderTarget () {
   const element = document.createElement('div');
   element.className = 'cycletest';
@@ -64,6 +81,51 @@ describe('TimeTravel', () => {
       $(renderTarget).find('.increment').trigger('click');
 
       assert.equal($(renderTarget).find('.count').text(), 'Count: 1');
+
+      done();
+    }, 1);
+  });
+
+  it('time travels', (done) => {
+    function main ({DOM}) {
+      const count$ = DOM.select('.increment').events('click')
+        .scan((count, _) => count + 1, 0)
+        .startWith(0);
+
+      const vtree$ = count$.map(count => (
+        h('.app', [
+          h('button.increment', 'Increment'),
+          h('.count', `Count: ${count}`)
+        ])
+      ));
+
+      return {
+        DOM: vtree$
+      };
+    }
+
+    const renderTarget = createRenderTarget();
+    TimeTravel.run(main, {DOM: makeDOMDriver(renderTarget)});
+
+    setTimeout(() => {
+      $(renderTarget).find('.increment').trigger('click');
+
+      assert.equal($(renderTarget).find('.count').text(), 'Count: 1');
+
+      $('.pause').trigger('click');
+
+      $('.stream').trigger('mousedown');
+
+      const streams = document.querySelectorAll('.stream');
+      const lastStream = streams[streams.length - 1];
+
+      lastStream.dispatchEvent(new MouseEvent('mousemove', {clientX: 500, clientY: 0}));
+
+      lastStream.dispatchEvent(new MouseEvent('mousedown'));
+
+      lastStream.dispatchEvent(new MouseEvent('mousemove', {clientX: 0, clientY: 0}));
+
+      assert.equal($(renderTarget).find('.count').text(), 'Count: 0');
 
       done();
     }, 1);
